5d755b2a9611f971040b06ba2e2aed3e4439b2b0
awa <etherify@proton.me>
2023-09-11 14:30:05 -0300

    Add AstroCompress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 import { defineConfig } from "astro/config";
 
+// Integrations.
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		tailwind({
 			applyBaseStyles: false, // Manually import .css file.
+		}),
+		compress({
+			CSS: false, // PostCSS already minifies CSS.
+			HTML: true,
+			Image: false,
+			JavaScript: false,
+			SVG: false,
 		}),
 	],
 });
